802e1722e6093a59b26015c5e3896c0daa3c2c1f
Giannis Kazakidis <giannis.kazakidis@valtech.com>
2024-07-05 01:07:11 +0200

    *Added animejs library *Add player card animation *Add logo animation

--- components/TeamOverview.vue
+++ components/TeamOverview.vue
@@ -1,17 +1,18 @@
 import type { Style } from '#build/components';
 <script setup>
-const selections = useSelections();
-let team = ref({});
-let roster = ref([]);
-let coach = ref([]);
+const { $anime } = useNuxtApp()
+const selections = useSelections()
+let team = ref({})
+let roster = ref([])
+let coach = ref([])
 
 if (selections.teamInfo) {
-	team = selections.teamInfo;
+  team = selections.teamInfo
 } else {
-	const { data } = await selections.fetchApi({
-		clubCode: selections.selectedTeam,
-	});
-	team = data.value;
+  const { data } = await selections.fetchApi({
+    clubCode: selections.selectedTeam,
+  })
+  team = data.value
 }
 // const { pending, data: people } = await useAsyncData(
 // 	'people',
@@ -25,99 +26,93 @@ if (selections.teamInfo) {
 // );
 
 const { data: people } = await selections.fetchApi({
-	clubCode: selections.selectedTeam,
-	competitionCode: selections.selectedLeague,
-	seasonCode: true,
-	query: 'people',
-});
+  clubCode: selections.selectedTeam,
+  competitionCode: selections.selectedLeague,
+  seasonCode: true,
+  query: "people",
+})
 
 if (people.value) {
-	roster = people.value.filter((person) => {
-		return person.typeName === 'Player';
-	});
-	coach = people.value.find((person) => {
-		return person.typeName === 'Coach';
-	});
-	selections.playersRes = roster;
+  roster = people.value.filter((person) => {
+    return person.typeName === "Player"
+  })
+  coach = people.value.find((person) => {
+    return person.typeName === "Coach"
+  })
+  selections.playersRes = roster
 }
+
+onMounted(() => {
+  $anime({
+    targets: ".player-card",
+    translateY: [100, 0],
+    delay: $anime.stagger(50, { start: 0 }),
+    easing: "cubicBezier(.42, 0, .58, 1)",
+  })
+})
 </script>
 
 <template>
-	<section class="relative viewport">
-		<div class="bg-team"></div>
-		<div
-			v-if="team"
-			class="py-8 mx-auto w-fit flex flex-col items-center gap-4 z-10"
-		>
-			<img
-				:src="team.images.crest"
-				:alt="team.name"
-				@load="getImageColor"
-				class="w-40 select-none drop-shadow-[0_35px_35px_rgba(255,255,255,0.35)]"
-			/>
-			<h2 class="font-chakra text-white text-xl drop-shadow-xl">
-				{{ team.name }}
-			</h2>
-		</div>
-		<div class="container mb-4">
-			<p
-				v-if="coach"
-				class="text-zinc-700 font-bold drop-shadow-[0_35px_35px_rgba(255,255,255,0.35)]"
-			>
-				<span v-if="coach.person.name"
-					>Head Coach - {{ coach.person.name }}</span
-				>
-			</p>
-		</div>
+  <section class="relative viewport">
+    <div class="bg-team"></div>
+    <div v-if="team" class="py-8 mx-auto w-fit flex flex-col items-center gap-4 z-10">
+      <img
+        :src="team.images.crest"
+        :alt="team.name"
+        @load="getImageColor"
+        class="w-40 select-none drop-shadow-[0_35px_35px_rgba(255,255,255,0.35)]"
+      />
+      <h2 class="font-chakra text-white text-xl drop-shadow-xl">
+        {{ team.name }}
+      </h2>
+    </div>
+    <div class="container mb-4">
+      <p v-if="coach" class="text-zinc-700 font-bold drop-shadow-[0_35px_35px_rgba(255,255,255,0.35)]">
+        <span v-if="coach.person.name">Head Coach - {{ coach.person.name }}</span>
+      </p>
+    </div>
 
-		<div class="container overflow-hidden text-primary py-3 mb-4">
-			<ul class="flex flex-wrap -mx-3">
-				<li
-					v-for="player in roster"
-					class="px-3 mb-6 w-full max-h-[600px] md:w-1/2 lg:w-1/4"
-				>
-					<div class="card w-full h-full glass">
-						<figure>
-							<img
-								v-if="player.images.headshot"
-								:src="player.images.headshot"
-								:alt="player.person.name"
-								class="max-h-full object-contain"
-							/>
-							<img
-								v-else
-								src="/assets/player-image.webp"
-								:alt="player.person.name"
-								class="h-52 mt-4 md:mt-12 object-contain"
-							/>
-						</figure>
-						<div class="card-body p-4 lg:p-8">
-							<h2 class="card-title">{{ player.person.name }}</h2>
-							<p>
-								<span v-if="player.dorsal"
-									>#{{ player.dorsal }}</span
-								>
-								<span v-if="player.positionName">
-									- {{ player.positionName }}
-								</span>
-							</p>
+    <div class="container overflow-hidden text-primary py-3 mb-4">
+      <ul class="flex flex-wrap -mx-3">
+        <li v-for="player in roster" class="player-card px-3 mb-6 w-full max-h-[600px] md:w-1/2 lg:w-1/4">
+          <div class="card w-full h-full glass">
+            <figure>
+              <img
+                v-if="player.images.headshot"
+                :src="player.images.headshot"
+                :alt="player.person.name"
+                class="max-h-full object-contain"
+              />
+              <img
+                v-else
+                src="/assets/player-image.webp"
+                :alt="player.person.name"
+                class="h-52 mt-4 md:mt-12 object-contain"
+              />
+            </figure>
+            <div class="card-body p-4 lg:p-8">
+              <h2 class="card-title">{{ player.person.name }}</h2>
+              <p>
+                <span v-if="player.dorsal">#{{ player.dorsal }}</span>
+                <span v-if="player.positionName"> - {{ player.positionName }} </span>
+              </p>
 
-							<div class="card-actions justify-end">
-								<NuxtLink
-									:to="{
-										name: 'players-id',
-										params: { id: player.person.code },
-									}"
-									class="btn btn-primary"
-									>See stats</NuxtLink
-								>
-							</div>
-						</div>
-					</div>
-				</li>
-			</ul>
-		</div>
-	</section>
+              <div class="card-actions justify-end">
+                <NuxtLink
+                  :to="{
+                    name: 'players-id',
+                    params: { id: player.person.code },
+                  }"
+                  class="btn btn-primary"
+                  >See stats</NuxtLink
+                >
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
 
 <style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,55 @@
-<script lang="ts" setup>
-onBeforeMount(() => getMainBackground());
+<script setup>
+onBeforeMount(() => getMainBackground())
+const { $anime } = useNuxtApp()
+
+onMounted(() => {
+  const tl = $anime.timeline({
+    easing: "easeOutExpo",
+    duration: 800,
+  })
+
+  tl.add({
+    targets: ".basketball",
+    translateY: function () {
+      const hero = document.querySelector(".hero")
+      const distance = Math.ceil(hero.offsetHeight / 2)
+      return [distance, 0]
+    },
+    easing: "easeOutElastic",
+  })
+    .add(
+      {
+        targets: ".basketball",
+        opacity: [1, 0],
+        scale: [1, 3],
+      },
+      300
+    )
+    .add(
+      {
+        targets: ".logo",
+        opacity: [0, 1],
+      },
+      400
+    )
+})
 </script>
 
 <template>
-	<div class="hero relative viewport">
-		<div class="bg-main"></div>
-		<div
-			class="hero-content p-8 container overflow-hidden flex-col lg:flex-row"
-		>
-			<img
-				src="/logo.png"
-				alt="Logo"
-				class="max-w-xs drop-shadow-[0_0px_8px_rgba(255,255,255,0.2)]"
-			/>
-			<div class="hero-text-bg relative my-8 mx-[calc(1%+2rem)]">
-				<h1 class="text-5xl font-chakra">Basketball database</h1>
-				<p class="text-lg py-6">
-					This is a basketball database aiming to give you stats from
-					the European leagues.
-				</p>
-				<NuxtLink to="/leagues" class="btn btn-primary"
-					>Get Started</NuxtLink
-				>
-			</div>
-		</div>
-	</div>
+  <div class="hero relative viewport overflow-hidden">
+    <div class="bg-main"></div>
+    <div class="hero-content p-8 container flex-col lg:flex-row">
+      <div class="relative">
+        <img src="/assets/basketball.svg" alt="" class="basketball absolute opacity-0 top-[75px] left-[48%] w-8 h-8" />
+        <img src="/logo.png" alt="Logo" class="logo opacity-0 max-w-xs drop-shadow-[0_0px_8px_rgba(255,255,255,0.2)]" />
+      </div>
+      <div class="hero-text-bg relative my-8 mx-[calc(1%+2rem)]">
+        <h1 class="text-5xl font-chakra">Basketball database</h1>
+        <p class="text-lg py-6">This is a basketball database aiming to give you stats from the European leagues.</p>
+        <NuxtLink to="/leagues" class="btn btn-primary">Get Started</NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
